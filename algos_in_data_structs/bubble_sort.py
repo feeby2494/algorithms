c886d7aec1a8e72c30415b4e6ba349bd9185194d
Jamie Lynn <toby2494@hotmail.com>
2022-12-22 11:00:23 -0600

    Added bubble sort

---
+++ algos_in_data_structs/bubble_sort.py
@@ -0,0 +1,13 @@
+# Bubble sort - Optimized vs Unoptimized
+
+def bubble_optimitzed(A):
+    iterations = 0
+    for i in range(len(A)):
+        for j in range(len(A) - i - 1):
+            iterations += 1
+            if A[j] > A[j+1]:
+                A[j], A[j+1] = A[j+1], A[j]
+    return A, iterations
+
+A = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+print(bubble_optimitzed(A))
